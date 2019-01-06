d89e79614c3ed79e2dc0f613a1c86b06955693d4
pratikw1859 <pratikwankhede814@gmail.com>
2019-01-06 10:55:07 +0530

    added UrlShortnerController.java

---
+++ spring-boot-redis-for-url-shortner-git/src/main/java/com/app/SpringBootRedisForUrlShortnerGitApplication.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisForUrlShortnerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisForUrlShortnerGitApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-redis-for-url-shortner-git/src/main/java/com/app/rest/controller/UrlShortnerController.java
@@ -0,0 +1,40 @@
+package com.app.rest.controller;
+
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.hash.Hashing;
+
+@RestController
+public class UrlShortnerController {
+
+	private RedisTemplate<String,String> redisTemplate;
+	
+	public UrlShortnerController(RedisTemplate<String, String> redisTemplate) {
+		super();
+		this.redisTemplate = redisTemplate;
+	}
+
+	@PostMapping
+	public String createShortUrl(@RequestBody String url){
+		//Validate The URL using UrlValidator present in Common-validator
+		UrlValidator validator = new UrlValidator(new String[] {"http","https"});
+		if(validator.isValid(url)) {
+			/*
+			 * If Url is Valid Then Do Hahing using Murmur_3 provided by Google Gauva
+			 */
+			String id = Hashing.murmur3_128().hashString(url, StandardCharsets.UTF_16).toString();
+			/*
+			 * Store Id in Redis as a Key and Url as Value
+			 */
+			redisTemplate.opsForValue().set(id, url);
+			return id;
+		}
+		throw new RuntimeException("Invalid Url...");
+	}
+}
